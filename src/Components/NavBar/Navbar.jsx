ece5c511f94ac2ada0166ea33e0dd8b6237a4efb
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-02-28 06:27:52 +0100

    Fixed  navbar issue

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -10,23 +10,23 @@ import { useState } from "react";
 export const Navbar = () => {
     const [isShowNav, setIsShowNav] = useState(true);
     const [isHideNav, setIsHideNav] = useState(false);
-    const [isNav, setIsNav] = useState('[-1000px]');
+    const [isNav, setIsNav] = useState(-1000);
 
     const menuClicked = () => {
         setIsHideNav(true);
         setIsShowNav(false);
-        setIsNav('0');
+        setIsNav(0);
     }
     const closeMenuClicked = () => {
         setIsHideNav(false);
         setIsShowNav(true);
-        setIsNav('[-1000px]');
+        setIsNav(-1000);
     }
     return(
         <>
         <nav className="bg-[#000C23] flex items-center justify-around p-[10px]">
             <div><Link to='/' className="uppercase font-[900] text-[25px]  text-[#fff] z-10 relative"> AllFurniture</Link></div>
-            <ul className={`flex-col items-start gap-[25px] md:gap-[35px] flex md:flex-row md:justify-center fixed bg-[#000C23] duration-500 ease-in animation left-${[isNav]} md:left-0 w-[50%] top-0 px-[20px] md:px-0   py-[100px] bottom-0 md:py-[10px] md:relative`}>
+            <ul className={`flex-col items-start gap-[25px] md:gap-[35px] flex md:flex-row md:justify-center fixed bg-[#000C23] duration-500 ease-in animation left-[${isNav}px] md:left-0 w-[50%] top-0 px-[20px] md:px-0   py-[100px] bottom-0 md:py-[10px] md:relative`}>
                 <li><Link to='/' className="text-[20px]   font-[400] text-[#fff] ">Home</Link></li>
                 <li><Link to='/modern' className="text-[20px]  font-[400] text-[#fff] ">Modern Furniture</Link></li>
                 <li><Link to='/art' className="text-[20px]  font-[400] text-[#fff] ">Art Design Furniture</Link></li>
